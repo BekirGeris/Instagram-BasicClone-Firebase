e2fad4aec3bea28e2f78f4316eb6cc92c27d5c10
BekirGeris <48486272+BekirGeris@users.noreply.github.com>
2021-10-13 23:09:48 +0300

    +görsellerin firebase storage ye eklenebiliyor.

--- app/src/main/java/com/begers/instagramclone/UploadActivity.java
+++ app/src/main/java/com/begers/instagramclone/UploadActivity.java
@@ -4,6 +4,7 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -22,7 +23,16 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.begers.instagramclone.databinding.ActivityUploadBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.UUID;
 
 public class UploadActivity extends AppCompatActivity {
 
@@ -31,6 +41,11 @@ public class UploadActivity extends AppCompatActivity {
     private Uri imageData;
 
     private ActivityUploadBinding binding;
+
+    private FirebaseStorage firebaseStorage;
+    private FirebaseAuth auth;
+    private FirebaseFirestore firebaseFirestore;
+    private StorageReference storageReference;
 
     /* Bitmape dönüştürmek
     private Bitmap selectedImage;
@@ -46,10 +61,31 @@ public class UploadActivity extends AppCompatActivity {
 
 
         registerLauncher();
+
+        firebaseStorage = FirebaseStorage.getInstance();
+        auth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        storageReference = firebaseStorage.getReference();
     }
 
     public void uploadButtonClicked(View view){
 
+        if (imageData != null){
+            //universal unique id
+            UUID uuid = UUID.randomUUID();
+            String imageName = "images/" + uuid + ".jpg";
+            storageReference.child(imageName).putFile(imageData).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(UploadActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+        }
     }
 
     public void selectImage(View view){
